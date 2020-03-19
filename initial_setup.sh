5f843cf8d5d8c1d47505894e8ead10ff54cccb42
Kyle Travis <kyle@kyletravis.com>
2020-03-19 16:26:00 +0000

    added default editor

--- initial_setup.sh
+++ initial_setup.sh
@@ -112,6 +112,10 @@ net.ipv6.conf.default.accept_redirects = 0
 net.ipv4.icmp_echo_ignore_all = 1
 EOF
 
+# update editor
+log updating default editor to vi
+update-alternatives --set editor /usr/bin/vim.basic
+
 # configure unattended upgrades
 log configuring unattended upgrades
 
